06d971271f0e976f6f675665dd9650ba4e91eb4e
ekowhyn <41103606+ekowhyn@users.noreply.github.com>
2018-09-08 08:50:28 +0700

    Add files via upload

---
+++ analogy.js
@@ -0,0 +1,5 @@
+<script src='https://cdn.rawgit.com/ekowhyn/ekowhyn/c5303198/newtechnology.js' type='text/javascript'/>
+<script type='text/javascript'>
+protected_links = &quot;domain.com,blogspot.com,blogger.com,linkedin.com,facebook.com,twitter.com,google.com,feedburner.com&quot;;
+auto_safelink();
+</script>
